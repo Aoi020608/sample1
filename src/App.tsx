e79264f13da0177ec97f2047e21374bfcdaecce4
Aoi <ht.so.ik0102@gmail.com>
2021-09-12 19:59:07 +0700

    change todo.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<{ id: string; text: string }[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
     setTodos([{ id: Math.random().toString(), text: text }]);
