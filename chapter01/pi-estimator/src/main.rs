ebe01a5ffe9c5e8f73be5e90ccb45f110b4f4495
Aoi <ht.so.ik0102@gmail.com>
2022-03-13 22:21:15 +0700

    test successfully

--- chapter01/pi-estimator/src/main.rs
+++ chapter01/pi-estimator/src/main.rs
@@ -1,3 +1,30 @@
+// declare the module by its file name
+mod rounding;
+
+// Rust will also accept if you implement it right away
+mod printer {
+    // import a function from external crate (no more extern declaration required!)
+    use rustpilib::monte_carlo_pi;
+
+    // crates present in the parent can be imported using the crate prefix
+    use crate::rounding::round;
+
+    // internal crates can always be imported using the crate
+    pub fn pretty_print_pi_approx(iterations: usize) {
+        let pi = monte_carlo_pi(iterations);
+        let places: usize = 2;
+        println!(
+            "Pi is ~ {} and rounded to {} places {}",
+            pi,
+            places,
+            round(pi, places)
+        );
+    }
+}
+
+// import from the module above
+use printer::pretty_print_pi_approx;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_print_pi_approx(100_000);
 }
--- chapter01/rustpilib/src/lib.rs
+++ chapter01/rustpilib/src/lib.rs
@@ -19,9 +19,33 @@ pub fn monte_carlo_pi(iterations: usize) -> f32 {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    fn is_reasonably_pi(pi: f32) -> bool {
+        pi >= 3_f32 && pi <= 4.5_f32
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_monte_carlo_pi_1() {
+        let pi = monte_carlo_pi(1);
+        assert!(pi == 0_f32 || pi == 4_f32);
+    }
+
+    #[test]
+    fn test_monte_carlo_pi_500() {
+        let pi = monte_carlo_pi(500);
+        assert!(is_reasonably_pi(pi));
+    }
+
+    #[test]
+    fn test_monte_carlo_pi_1000() {
+        let pi = monte_carlo_pi(1000);
+        assert!(is_reasonably_pi(pi));
+    }
+
+    #[test]
+    fn test_monte_carlo_pi_5000() {
+        let pi = monte_carlo_pi(5000);
+        assert!(is_reasonably_pi(pi));
     }
 }
