36a059ff1d59eba6534b49cb92f903c183bd573f
aoi <62386689+Aoi1011@users.noreply.github.com>
2022-11-16 20:40:26 +0700

    Delete target directory

--- target/debug/deps/solana_hello_world-fb4fa792871115f8.d
+++
@@ -1,5 +0,0 @@
-/root/Document/solana/solana-hello-world/target/debug/deps/solana_hello_world-fb4fa792871115f8.rmeta: src/lib.rs
-
-/root/Document/solana/solana-hello-world/target/debug/deps/solana_hello_world-fb4fa792871115f8.d: src/lib.rs
-
-src/lib.rs:
